24a6b44440ec6830782689e09de3339a9cc8fe1b
donald1623 <donaldaudriel@gmail.com>
2020-03-22 16:38:30 +0100

    added post endpoint to db

--- routes/todos.js
+++ routes/todos.js
@@ -4,8 +4,8 @@ var mongojs = require("mongojs");
 var db = mongojs("mytodos", ["todos"]);
 
 
-
-router.get("/todos", (req, res, next)=>{ //get request to todos database
+//get request to all todos in the database
+router.get("/todos", (req, res, next)=>{ 
     db.todos.find( (err, todos) =>{
       if(err){
           res.send(err);
@@ -14,6 +14,24 @@ router.get("/todos", (req, res, next)=>{ //get request to todos database
     });
 });
 
+//post request to add todo to database
+router.post("/todo", (req, res, next)=>{
+     var todo = req.body;
+     console.log(todo);
+     if(!todo.title){
+         res.status(400);
+         res.json({
+             error : "inputed wrong data"
+         });
+     } else {
+         db.todos.save(todo, (err, todo)=>{
+           if(err){
+               res.send(err);
+           }
+           res.json(todo);
+         });
+     }
+});
 
 
 module.exports = router;
